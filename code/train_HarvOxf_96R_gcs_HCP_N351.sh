4e3e1246666c6ec066d031d308e0e01e3293db3a
Kyesam <gssure@gmail.com>
2021-06-13 18:16:08 +0200

    Add comments

--- code/train_HarvOxf_96R_gcs_HCP_N351.sh
+++ code/train_HarvOxf_96R_gcs_HCP_N351.sh
@@ -12,18 +12,38 @@ atl=HarvardOxford/HarvardOxford-cortl-maxprob-thr0-1mm.nii.gz
 mni_brain=/usr/share/fsl/5.0/data/standard/MNI152_T1_1mm_brain.nii.gz
 tmp=${tp}/${grp}/${sbj}/temp
 
-mkdir -p ${tmp}
+# Colors
+# ------
+RED='\033[1;31m'	# Red
+GRN='\033[1;32m' 	# Green
+NCR='\033[0m' 		# No Color
+
+# Temporary folder check
+# ----------------------
+if [[ -d ${tmp} ]]; then
+	printf "${GRN}[Unix]${RED} ID: ${grp}${sbj}${NCR} - Temporary folder exists, so the process will overwrite the files in the target folder.\n"
+else
+	printf "${GRN}[Unix]${RED} ID: ${grp}${sbj}${NCR} - Create a temporary folder.\n"
+	mkdir -p ${tmp}
+fi
+
 mri_convert ${fp}/${grp}_${sbj}/mri/brain.mgz ${tmp}/fs_t1_brain.nii.gz
-flirt -ref ${mni_brain} -in ${tmp}/fs_t1_brain.nii.gz -omat ${tp}/${grp}/${sbj}/fs_t1_to_mni_affine.mat -dof 6
+
+# Linear transformation from T1-weigted image to the MNI152 T1 1mm
+# --------------------------------------------------------------------
+flirt -ref ${mni_brain} -in ${tmp}/fs_t1_brain.nii.gz -omat ${tp}/${grp}/${sbj}/fs_t1_to_mni_affine.mat -dof 12
+
+# Non-linear transformation from T1-weigted image to the MNI152 T1 1mm
+# --------------------------------------------------------------------
 fnirt --in=${tmp}/fs_t1_brain.nii.gz --aff=${tp}/${grp}/${sbj}/fs_t1_to_mni_affine.mat --cout=${tp}/${grp}/${sbj}/fs_t1_to_mni_warp_struct.nii.gz --config=T1_2_MNI152_2mm
+
+# Apply the deformation to the atlas on the MNI152 T1
+# ---------------------------------------------------
 invwarp --ref=${tmp}/fs_t1_brain.nii.gz --warp=${tp}/${grp}/${sbj}/fs_t1_to_mni_warp_struct.nii.gz --out=${tp}/${grp}/${sbj}/mni_to_fs_t1_warp_struct.nii.gz
 applywarp --ref=${tmp}/fs_t1_brain.nii.gz --in=${ap}/${atl} --warp=${tp}/${grp}/${sbj}/mni_to_fs_t1_warp_struct.nii.gz --out=${tp}/${grp}/${sbj}/HO_to_fs_t1.nii.gz --interp=nn
-# applywarp --ref=${tp}/${grp}/${sbj}/fs_t1_brain.nii.gz --in=${ap}/${atl} --out=${tp}/${grp}/${sbj}/HO_to_fs_t1.nii.gz --warp=${tp}/${grp}/${sbj}/mni_to_fs_t1_warp_struct.nii.gz --interp=nn
 
-# fslreorient2std -m reori_fs_t1.mat ${tmp}/fs_t1_brain.nii.gz
-# convert_xfm -omat reori_fs_t1_inv.mat -inverse reori_fs_t1.mat
-# applywarp -i HO_to_fs_t1.nii.gz -r temp_ref.nii.gz -o HO_to_fs_t1.nii.gz --premat=reori_fs_t1_inv.mat --interp=nn 
-
+# Create annotation files (lh and rh)
+# -----------------------------------
 mri_convert ${tp}/${grp}/${sbj}/HO_to_fs_t1.nii.gz ${fp}/${grp}_${sbj}/mri/HarvardOxford_96R.mgz 
 mris_sample_parc -ct ${ap}/HO_R96_color_table.txt -sdir ${fp} ${grp}_${sbj} rh HarvardOxford_96R.mgz rh.HarvardOxford_96R.annot
 mris_sample_parc -ct ${ap}/HO_R96_color_table.txt -sdir ${fp} ${grp}_${sbj} lh HarvardOxford_96R.mgz lh.HarvardOxford_96R.annot
