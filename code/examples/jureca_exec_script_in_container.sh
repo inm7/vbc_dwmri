57a05f212e58069b883fe704bc0d0d333a2229ca
Kyesam <gssure@gmail.com>
2021-06-26 12:32:38 +0200

    Add step 3

--- code/examples/jureca_exec_script_in_container.sh
+++ code/examples/jureca_exec_script_in_container.sh
@@ -4,7 +4,6 @@ grp=${2}
 startNum=${3}
 endNum=${4}
 maxTasks=${5}
-wp=$(pwd)
 
 VBC_DWMRI='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/vbc_dwmri_v1.3.simg'
 FREESURFER_LICENSE='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/license.txt'
@@ -12,20 +11,53 @@ FREESURFER_LICENSE='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/license.t
 SET_FP='/p/project/cjinm71/SC_Pipe_jung3/Neuroimage/Tools/freesurfer/subjects'
 SET_TP='/p/scratch/cjinm71/jung3/03_Structural_Connectivity'
 SET_AP='/p/project/cjinm71/SC_Pipe_jung3/Neuroimage/Atlas'
-SCRIPT='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/vbc_dwmri/code/examples/train_HarvOxf_96R_gcs_step_2.sh'
 
-nTask=0
+# train_HarvOxf_96R_gcs_step_1.sh
+# -------------------------------
+# SCRIPT='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/vbc_dwmri/code/examples/train_HarvOxf_96R_gcs_step_1.sh'
+# nTask=0
+# for (( i = startNum; i < endNum + 1 ; i++ )); do
+#     sbj=$(sed -n ${i}p ${fn})
+#     ARGUMENTS="${grp} ${sbj}"
+# 	  printf "singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &\n"
+#     singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &
+#     (( nTask++ ))
+#     if [[ ${nTask} -eq ${maxTasks} ]]; then
+#         wait
+#         nTask=0
+#     fi
+# done
+# wait
+
+# train_HarvOxf_96R_gcs_step_2.sh
+# -------------------------------
+# SCRIPT='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/vbc_dwmri/code/examples/train_HarvOxf_96R_gcs_step_2.sh'
+# nTask=0
+# for (( i = startNum; i < endNum + 1 ; i++ )); do
+#     sbj=$(sed -n ${i}p ${fn})
+#     ARGUMENTS="${grp} ${sbj}"
+# 	  printf "singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &\n"
+#     singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &
+#     (( nTask++ ))
+#     if [[ ${nTask} -eq ${maxTasks} ]]; then
+#         wait
+#         nTask=0
+#     fi
+# done
+# wait
+
+# train_HarvOxf_96R_gcs_step_3.sh
+# -------------------------------
+SCRIPT='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/vbc_dwmri/code/examples/train_HarvOxf_96R_gcs_step_3.sh'
 for (( i = startNum; i < endNum + 1 ; i++ )); do
     sbj=$(sed -n ${i}p ${fn})
-    ARGUMENTS="${grp} ${sbj}"
-    
-	printf "singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &\n"
-    singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &
-    (( nTask++ ))
-    if [[ ${nTask} -eq ${maxTasks} ]]; then
-        wait
-        nTask=0
+    if [[ ${i} -eq 1 ]]; then
+        ARGUMENTS="${grp}_${sbj}"
+    else
+        ARGUMENTS+=" ${grp}_${sbj}"
     fi
 done
 wait
 
+printf "singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &\n"
+# singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &
