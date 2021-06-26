61adb5557f5d99c9eb1b0f79a8c44125922c72ca
Kyesam <gssure@gmail.com>
2021-06-26 02:09:03 +0200

    Debug

--- code/examples/jureca_exec_script_in_container.sh
+++ code/examples/jureca_exec_script_in_container.sh
@@ -13,13 +13,13 @@ SET_FP='/p/project/cjinm71/SC_Pipe_jung3/Neuroimage/Tools/freesurfer/subjects'
 SET_TP='/p/scratch/cjinm71/jung3/03_Structural_Connectivity'
 SET_AP='/p/project/cjinm71/SC_Pipe_jung3/Neuroimage/Atlas'
 SCRIPT='/p/project/cjinm71/Jung/01_MRI_pipelines/Container/vbc_dwmri/code/examples/train_HarvOxf_96R_gcs_step_2.sh'
-ARGUMENTS="${grp} ${sbj}"
 
 nTask=0
-for (( i = startNum; i < totalNum + 1 ; i++ )); do
+for (( i = startNum; i < endNum + 1 ; i++ )); do
     sbj=$(sed -n ${i}p ${fn})
-
-	printf "singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${grp} ${sbj} &\n"
+    ARGUMENTS="${grp} ${sbj}"
+    
+	printf "singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &\n"
     singularity exec --cleanenv -B ${SET_TP}:/mnt_tp,${SET_FP}:/mnt_fp,${SET_AP}:/mnt_ap,${FREESURFER_LICENSE}:/opt/freesurfer/license.txt,${SCRIPT}:/opt/script.sh ${VBC_DWMRI} /opt/script.sh ${ARGUMENTS} &
     (( nTask++ ))
     if [[ ${nTask} -eq ${maxTasks} ]]; then
